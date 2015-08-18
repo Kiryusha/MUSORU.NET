89570603f60aa22bd5644f0fcd3b82f9b7ff0ef0
Kir Kostin <gimun0@gmail.com>
2015-08-18 04:19:35 +0300

    Форма и попап хедера

--- js/script.js
+++ js/script.js
@@ -3,9 +3,10 @@ $(document).ready(function () {
 	/* Субменю в хедере */
 
 	var slideAllow = true;
-	function tglMainSubmenu() {
+
+	function toggleSmth(someItem, someDir, someSpeed) {
 		if (slideAllow)	{
-			$('.js-nav-submenu').toggle('slide',  {direction: 'up'}, 300);
+			$(someItem).toggle('slide',  {direction: someDir || 'up'}, someSpeed || 300);
 			slideAllow = false;
 		}
 		setTimeout(function() {slideAllow = true}, 500);
@@ -13,54 +14,97 @@ $(document).ready(function () {
 
 	$('.js-nav-link').hoverIntent(function() {
 		if (!$('.js-nav-submenu').is(':visible')) {
-			tglMainSubmenu();
+			toggleSmth('.js-nav-submenu', 'up');
 		}
 	}, function() {return true});
 
 	$('.js-nav-wrapper').on('mouseleave', function() {
 		if ($('.js-nav-submenu').is(':visible')) {
-			tglMainSubmenu();
+			toggleSmth('.js-nav-submenu', 'up');
 		}
 	});
 
 	$('.js-submenu-close').click(function(){
-		tglMainSubmenu();
+		toggleSmth('.js-nav-submenu', 'up');
 	});
+
+	/* Попап в хедере */
+
+	$('.header .js-popup-open').click(function() {
+		toggleSmth('.js-header-form-wrapper', 'right', 600);
+	});
+
+	if ($('.js-header-form-wrapper').length) {
+		$(document).click(function(event) {
+			if ($('.js-header-form-wrapper').is(':visible')) {
+				if(!$(event.target).closest('.js-header-form-wrapper, .js-popup-open').length) {
+					toggleSmth('.js-header-form-wrapper', 'right', 600);
+				}
+			}
+		});
+	}
+
+	$('.header .js-popup-close').click(function() {
+		toggleSmth('.js-header-form-wrapper', 'right', 600);
+	});
+
+	/* Форма */
+
+	$('.js-fancy-field').click(function() {
+		$('.js-fancy-field').removeClass('is-focused');
+		$('input', $('.js-fancy-field')).each(function() {
+			if (!$(this).val()) {
+				$(this).parent().removeClass('has-label')
+			}
+		});
+		if (!$(this).hasClass('is-focused')) {
+			$(this).addClass('is-focused').addClass('has-label');
+		}
+	});
+
+	if ($('.js-fancy-field').length) {
+		$(document).click(function(event) {
+			if(!$(event.target).closest('.js-fancy-field').length) {
+				if ($('.js-fancy-field').hasClass('is-focused')) {
+					$('.js-fancy-field').removeClass('is-focused');
+				}
+				$('input', $('.js-fancy-field')).each(function() {
+					if (!$(this).val()) {
+						$(this).parent().removeClass('has-label')
+					}
+				});
+			}
+		});
+	}
 
 	/* Валидация */
 
-	if ($('.js-index-form').length) {
+	if ($('.js-form').length) {
 
-		$('.js-index-form').each(function(){
-			var form = $(this);
-			$(this).ajaxForm({
-				beforeSubmit : function(){
-					$('.js-index-form-input', form).each(function(){
-						if( $(this).data('require') == true && $.trim($(this).val()) == '') {
-							$(this).addClass('form-error');
-						} else if( $.trim($(this).val()) != '' ) {
-							$(this).removeClass('form-error');
-						}
-					});
-					if($('input.form-error', form).length) {
-						return false;
-					};
-				},
+		$('.js-form').each(function(){
+			var $form = $(this);
+			$form.ajaxForm({
 				dataType : 'html',
 				success : function(){
-					if ($('.js-index-form').hasClass('gift-map')) {
-						$('.popup-wrapper--end').stop().fadeIn(300);
-						$('body').addClass('js-popup-open');
-						if ($(document).height() > $(window).height()) {
-							$('body').css({'padding-right' : scrollbarWidth + 'px'});
-						}
-					} else {
-						alert('Success!');
-					}
+					$('.js-form-content', $form).hide();
+					$('.js-fancy-field input', $form).val('');
+					$('.js-form-success', $form).fadeIn();
+					setTimeout(function(){
+						$('.js-form-success', $form).hide();
+						$('.js-form-content', $form).fadeIn();
+					}, 5000);
 				}
 			});
 
 		});
-	};
+	}
+
+	/* Телефон */
+
+	if($('.js-phone').length) {
+
+		$('.js-phone').mask("+7 999 999 99 99");
+
+	}
 
 });
